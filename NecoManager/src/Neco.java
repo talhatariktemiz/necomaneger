64f0d851d6f9960c4f9536e4172c5fef5cb0d90c
axi <axi@localhost>
2022-09-08 21:07:40 +0300

    bir takım eklemeler

--- NecoManager/src/Neco.java
+++ NecoManager/src/Neco.java
@@ -4,7 +4,7 @@ public class Neco {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("necooo");
+		System.out.println("necooo"+"bir takım denemeler");
 
 	}
 
