c98b4446486d2747451ec6ef20e4ca564aa18ec9
axi <axi@localhost>
2022-09-12 22:25:43 +0300

    addingNotes

--- NecoManager/src/Neco.java
+++ NecoManager/src/Neco.java
@@ -5,6 +5,10 @@ public class Neco {
 		// TODO Auto-generated method stub
 		
 		System.out.println("necooo"+"bir takım denemeler lütfen dikkat edini");
+		
+		for (int i=0; i<500;i++) {
+			System.out.println("Bekledim de gelmedin beni niye beklettin,");
+		}
 
 	}
 
